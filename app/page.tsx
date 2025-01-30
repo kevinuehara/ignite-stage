64609e920d16544d4c7fdc3fee0ca7865ae05023
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2025-01-30 14:29:19 -0300

    Adiciona classes do modo de cor

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,55 @@
+"use client";
 import { EventContainer } from "@/components/EventContainer";
 import { Header } from "@/components/Header";
 import { Events2024 } from "@/events/event-2024";
 import { Events2025 } from "@/events/event-2025";
+import { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import { TbSunHigh } from "react-icons/tb";
+import { LuMoonStar } from "react-icons/lu";
+
 export default function Home() {
+  const [isDarkOrLight, setIsDarkOrLight] = useState(
+    window.localStorage.getItem("isDarkOrLight") || "light"
+  );
+  const handleToggleLightOrDark = () => {
+    const newValue = isDarkOrLight === "light" ? "dark" : "light";
+    window.localStorage.setItem("isDarkOrLight", newValue);
+    setIsDarkOrLight(newValue);
+  };
+
   return (
-    <>
+    <main className={isDarkOrLight}>
       <Header />
+      <div className="flex flex-col gap-10 items-start justify-start">
+        <EventContainer title="December 2024" data={Events2024.DECEMBER} />
+        <EventContainer title="January 2025" data={Events2025.JANUARY} />
+        <EventContainer title="February 2025" data={Events2025.FEBRUARY} />
+      </div>
 
-      <EventContainer title="December 2024" data={Events2024.DECEMBER} />
-      <EventContainer title="January 2025" data={Events2025.JANUARY} />
-      <EventContainer title="February 2025" data={Events2025.FEBRUARY} />
-
-      <footer className="flex justify-center flex-col items-center">
-        <div className="flex flex-row items-center md:justify-start justify-center w-screen h-20 mt-5 bg-gradient-to-t from-violet-600 to-purple-900">
-          <div className="flex flex-col text-center md:text-start">
-            <p className="mt-2 md:ml-5 ml-0">
-              Created by Kevin Uehara and Community 2025
-            </p>
-          </div>
+      <footer className="mt-5 py-10">
+        <p className="text-center text-sm">
+          Created by{" "}
+          <a href="https://github.com/kevinuehara" target="_blank">
+            @kevinuehara
+          </a>{" "}
+          and Community 2025; <br /> Style Review by{" "}
+          <a href="https://github.com/paulhenrique">@phvcandido</a>.
+        </p>
+        <div className="mt-6 text-center flex justify-center text-sm">
+          <a target="_blank" href="https://github.com/kevinuehara/ignite-stage">
+            <FaGithub size="30px" />
+          </a>
         </div>
       </footer>
-    </>
+
+      <button className="lightButton" onClick={handleToggleLightOrDark}>
+        {isDarkOrLight === "light" ? (
+          <TbSunHigh size="24px" className="rotate" />
+        ) : (
+          <LuMoonStar size="24px" className="rotate" />
+        )}
+      </button>
+    </main>
   );
 }
